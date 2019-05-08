9d3234f06d54378af647a17ed756575fc3f828e0
Dhugald Spry (1000040435) <SPRYDL1@student.op.ac.nz>
2019-05-08 13:35:37 +1200

    Added name question

--- AI/AI/AIProgram.cs
+++ AI/AI/AIProgram.cs
@@ -34,9 +34,10 @@ namespace AI
         }
         public static void Dhugald()
         {
-
-
-
+            Console.WriteLine("What is your name? ");
+            string answer1 = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine(answer1);
 
 
         }
@@ -47,6 +48,9 @@ namespace AI
 
         static void Main()
         {
+            Dhugald();
+
+
         }
     }
 }
